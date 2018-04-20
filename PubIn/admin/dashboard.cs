a53dc6d7ea4cdf86b4aabd1c234e493ce7a5ca62
simon mbugua <smbugua11@gmail.com>
2018-04-20 20:31:28 +0300

    button panel fixed

--- PubIn/admin/dashboard.cs
+++ PubIn/admin/dashboard.cs
@@ -15,33 +15,45 @@ namespace PubIn.admin
         public dashboard()
         {
             InitializeComponent();
-            slidepanel.Height = button1.Height;
+            //slidepanel.Height = button1.Height;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             slidepanel.Height = button1.Height;
+            slidepanel.Top = button1.Top;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             slidepanel.Height = button2.Height;
+            slidepanel.Top = button2.Top;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             slidepanel.Height = button4.Height;
+            slidepanel.Top = button4.Top;
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            slidepanel.Height = button3.Height;
+            slidepanel.Top = button3.Top;
+
 
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            slidepanel.Height = button5.Height;
+            slidepanel.Top = button5.Top;
+        }
 
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
