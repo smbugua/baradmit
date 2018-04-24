e512d16ac957fde09639d8af36d09c693e4678d1
simon mbugua <smbugua11@gmail.com>
2018-04-24 16:35:45 +0300

    ok

--- PubIn/reports/reporter.Designer.cs
+++ PubIn/reports/reporter.Designer.cs
@@ -57,35 +57,36 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1262, 69);
+            this.panel1.Size = new System.Drawing.Size(946, 70);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(63)))), ((int)(((byte)(52)))));
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 640);
-            this.panel2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.panel2.Location = new System.Drawing.Point(0, 446);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1262, 33);
+            this.panel2.Size = new System.Drawing.Size(946, 23);
             this.panel2.TabIndex = 1;
             // 
             // eventname
             // 
             this.eventname.FormattingEnabled = true;
-            this.eventname.Location = new System.Drawing.Point(142, 27);
-            this.eventname.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.eventname.Location = new System.Drawing.Point(125, 21);
+            this.eventname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.eventname.Name = "eventname";
-            this.eventname.Size = new System.Drawing.Size(166, 29);
+            this.eventname.Size = new System.Drawing.Size(126, 29);
             this.eventname.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(23, 27);
+            this.label1.Location = new System.Drawing.Point(17, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(102, 21);
             this.label1.TabIndex = 1;
@@ -94,17 +95,18 @@
             // colorname
             // 
             this.colorname.FormattingEnabled = true;
-            this.colorname.Location = new System.Drawing.Point(464, 32);
-            this.colorname.Margin = new System.Windows.Forms.Padding(6);
+            this.colorname.Location = new System.Drawing.Point(371, 24);
+            this.colorname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.colorname.Name = "colorname";
-            this.colorname.Size = new System.Drawing.Size(166, 29);
+            this.colorname.Size = new System.Drawing.Size(126, 29);
             this.colorname.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(396, 35);
+            this.label2.Location = new System.Drawing.Point(297, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 21);
             this.label2.TabIndex = 3;
@@ -114,7 +116,8 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(344, 35);
+            this.label3.Location = new System.Drawing.Point(258, 24);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(26, 21);
             this.label3.TabIndex = 4;
@@ -125,9 +128,10 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(232)))), ((int)(((byte)(129)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(714, 16);
+            this.button1.Location = new System.Drawing.Point(534, 24);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 43);
+            this.button1.Size = new System.Drawing.Size(74, 30);
             this.button1.TabIndex = 5;
             this.button1.Text = "Filter";
             this.button1.UseVisualStyleBackColor = false;
@@ -138,9 +142,10 @@
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(64)))), ((int)(((byte)(198)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(898, 16);
+            this.button2.Location = new System.Drawing.Point(666, 24);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 43);
+            this.button2.Size = new System.Drawing.Size(74, 30);
             this.button2.TabIndex = 5;
             this.button2.Text = "Filter";
             this.button2.UseVisualStyleBackColor = false;
@@ -150,34 +155,37 @@
             // 
             this.panel3.Controls.Add(this.datagrid);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(0, 69);
+            this.panel3.Location = new System.Drawing.Point(0, 70);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1262, 571);
+            this.panel3.Size = new System.Drawing.Size(946, 376);
             this.panel3.TabIndex = 2;
             // 
             // datagrid
             // 
+            this.datagrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.datagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.datagrid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.datagrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(95)))), ((int)(((byte)(207)))));
             this.datagrid.Location = new System.Drawing.Point(0, 0);
+            this.datagrid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.datagrid.Name = "datagrid";
             this.datagrid.RowTemplate.Height = 24;
-            this.datagrid.Size = new System.Drawing.Size(1262, 571);
+            this.datagrid.Size = new System.Drawing.Size(946, 376);
             this.datagrid.TabIndex = 0;
             // 
             // reporter
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 33F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(84)))), ((int)(((byte)(96)))));
-            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.ClientSize = new System.Drawing.Size(946, 469);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "reporter";
             this.Text = "Reports Module";
             this.Load += new System.EventHandler(this.reporter_Load);
